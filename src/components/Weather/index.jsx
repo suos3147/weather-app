c21c2793e503767fa5f93ee7817828ccee5fd3fe
suos3147 <suos3147@gmail.com>
2020-05-04 22:24:46 +0900

    API키 분리

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,93 +1,79 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./style.css";
-import {
-  Sunny,
-  Cloudy,
-  Rainy,
-  Snow,
-  Storm,
-  Thermometer
-} from "../../assets/icon";
-const API_KEY = "33837eaf6c75e69f8d053726644db6e0";
+import {Thermometer} from "../../assets/icon";
+
+const API_WEATHER = process.env.REACT_APP_WEATHER_API_KEY;
+const API_KAKAO = process.env.REACT_APP_KAKAO_MAP_KEY;
 class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: {},
+      currentWeather: {},
       isLoading: false,
-      error: null
+      error: null,
     };
   }
 
-  getWeather = (lat, lon) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          current: data,
-          isLoading: false
-        });
-      })
-      .catch(error => this.setState({ error, isLoading: false }));
+  getAddress = (lat, lon) => {
+    fetch(`
+    http://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&language=ko
+    `)
+      .then((response) => response.json())
+      .then((data) => data);
   };
 
-  getPosition = cb => {
+  getWeather = (lat, lon) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_WEATHER}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          currentWeather: data,
+          isLoading: false,
+        });
+      })
+      .catch((error) => this.setState({error, isLoading: false}));
+  };
+
+  getPosition = (cb) => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        cb(latitude, longitude);
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const {latitude, longitude} = position.coords;
+          cb(latitude, longitude);
+        },
+        (error) => console.log("error", error)
+      );
     } else {
       alert("GPS를 사용할 수 없습니다.");
     }
   };
 
   async componentWillMount() {
-    this.setState({ isLoading: true });
+    this.setState({isLoading: true});
     await this.getPosition(this.getWeather);
   }
 
   render() {
-    const { current, isLoading } = this.state;
+    const {currentWeather, isLoading} = this.state;
     if (isLoading) {
       return <p className="app-main">Loading...</p>;
     }
 
     const {
-      main: { temp, temp_max, temp_min, feels_like },
-      weather: [{ id, description, icon }]
-    } = current;
-
-    // let icon = "";
-    // switch (id) {
-    //   case id < 300:
-    //     icon = <Storm className="weather-icon"></Storm>;
-    //     break;
-    //   case id < 600:
-    //     icon = <Rainy className="weather-icon"></Rainy>;
-    //     break;
-    //   case id < 700:
-    //     icon = <Snow className="weather-icon"></Snow>;
-    //     break;
-    //   case 800:
-    //     icon = <Sunny className="weather-icon"></Sunny>;
-    //     break;
-    //   case id < 900:
-    //     icon = <Cloudy className="weather-icon"></Cloudy>;
-    //     break;
-
-    //   default:
-    //     icon = "";
-    // }
+      main: {temp, temp_max, temp_min, feels_like},
+      weather: [{id, description, icon}],
+    } = currentWeather;
 
     return (
-      <div className="app-main main-weather">
+      <div className="app-main">
         <div className="current-weather">
           <div className="icon-box">
-            <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" />
-            {console.log(id)}
+            <img
+              src={`http://openweathermap.org/img/w/${icon}.png`}
+              alt={description}
+            />
             <span className="weather-text">{description}</span>
           </div>
           <div className="temp current-temp">
