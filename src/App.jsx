3c141aae343309aab7a4cb3c7ea98d85028fc251
suos3147 <suos3147@gmail.com>
2020-05-05 23:50:46 +0900

    Weather app 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ const App = () => {
   const [location, setLocation] = useState("");
   return (
     <div className="App">
-      <Header location={location} className="app-header"></Header>
-      <Weather setLocation={setLocation} className="app-main"></Weather>
-      <Footer className="app-footer"></Footer>
+      <section className="wrapper">
+        <Header location={location}></Header>
+        <Weather setLocation={setLocation}></Weather>
+      </section>
+      <Footer></Footer>
     </div>
   );
 };
