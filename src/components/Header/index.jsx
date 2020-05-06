f842eaec59bef08a0284580583ec9f985226992a
suos3147 <suos3147@gmail.com>
2020-05-07 00:23:34 +0900

    시계 초단위로 업데이트

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,47 +2,61 @@ import React, {Component} from "react";
 import "./style.css";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {now: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.setState({now: new Date()}), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
   render() {
     const location = this.props.location;
-    const date = new Date();
-    const month = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
+    const {now} = this.state;
     return (
       <header className="header">
         <section className="title">
           <span className="location">{location}</span>
           <div className="line"></div>
         </section>
-
         <section className="date">
-          <span>{month[date.getMonth()]} </span>
-          <span>{date.getDate()}, </span>
-          <span>{date.getFullYear()}</span>
+          <span>{month[now.getMonth()]} </span>
+          <span>{now.getDate()}, </span>
+          <span>{now.getFullYear()}</span>
         </section>
         <section className="time">
-          <span>{date.getHours()}</span>
-          <span>:</span>
           <span>
-            {date.getMinutes() < 10
-              ? `0${date.getMinutes()}`
-              : date.getMinutes()}
+            {now.getHours() < 10 ? `0${now.getHours()}` : now.getHours()}
+          </span>
+          :
+          <span>
+            {now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes()}
+          </span>
+          :
+          <span>
+            {now.getSeconds() < 10 ? `0${now.getSeconds()}` : now.getSeconds()}
           </span>
         </section>
       </header>
     );
   }
 }
+
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 export default Header;
